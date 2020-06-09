8c15688ab0ec6378fd63c78595dc6cb4fc4953a8
johnlomat <54453015+johnlomat@users.noreply.github.com>
2020-06-09 19:52:39 +0800

    Add animation on mobile-responsive navbar button.

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     $('nav > div > ul > li > a').click(function(){
         $('#navbarNav').removeClass('show');
+        $('.burger').toggleClass('active');
+    })
+    $('.burger').click(function(){
+        $('.burger').toggleClass('active');
     })
 });

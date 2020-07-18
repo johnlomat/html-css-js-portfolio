54516a70436029ff449d737bf609c21215ff1b75
johnlomat <54453015+johnlomat@users.noreply.github.com>
2020-07-19 07:44:21 +0800

    Create blurry.load.js

---
+++ assets/blurry-load/blurry.load.js
@@ -0,0 +1,13 @@
+(function ($) {
+    $.fn.blurryLoad = function (options) {
+        var parentContainer = $(this).parent();
+        var imageContainer = $(this);
+        var imgLarge = new Image();
+        
+        imgLarge.src = $(this).attr('data-large');
+        imgLarge.onload = function () {
+        	imageContainer.hide()
+            parentContainer.append(imgLarge);
+        };
+    };
+}(jQuery));

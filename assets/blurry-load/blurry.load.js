82f24af538f936a98acf1f5f55cbcc7d744271fd
johnlomat <54453015+johnlomat@users.noreply.github.com>
2020-07-19 19:53:18 +0800

    Update blurry.load.js

--- assets/blurry-load/blurry.load.js
+++ assets/blurry-load/blurry.load.js
@@ -9,7 +9,7 @@
              var timeout = setTimeout(function() {
                    imageContainer.hide();
                    parentContainer.append(imgLarge);
-              }, 2000);
+              }, 2500);
         });
     };
 }(jQuery));
